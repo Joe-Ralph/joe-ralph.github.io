515b37add6e93ad9e2fd3e1c828d7d5894af6b43
Joe Ralphin <joeral333@gmail.com>
2024-02-29 14:06:10 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -76,7 +76,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1a6RLYQy8XvubpbqQ7HWqKM-zJC_3xhpg/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
  skills: [
     'Docker',
